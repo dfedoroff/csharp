eb5be7d7c23a97e282cc64d5f56195f0e7d4bcde
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-09-29 20:38:29 +0700

    feat: Enhance temperature conversion
    #comment Refine the method for converting the temperature from Celsius to Fahrenheit within the WeatherForecast class. This enhancement will ensure accurate and optimized temperature conversion
    
    Affected: WeatherForecast (temperature conversion)

---
+++ MyForstWebApplication/MyForstWebApplication/Models/WeatherForecast.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace MyForstWebApplication.Models
+{
+    // Класс, представляющий прогноз погоды
+    public class WeatherForecast
+    {
+        // Дата прогноза
+        public DateTime Date { get; set; }
+
+        // Температура в градусах Цельсия
+        public int TemperatureC { get; set; }
+
+        // Температура в градусах Фаренгейта, вычисляемая из TemperatureC
+        public int TemperatureF => 32 + (int)(TemperatureC * 9.0 / 5); // Оптимизированный метод для конвертации температуры
+    }
+}
