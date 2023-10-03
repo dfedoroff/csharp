ea31e1ca364fe2b1ce575884184cfb9f5073567e
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-10-03 21:16:30 +0700

    feat: Add controller functionalities
    #comment Integrate Client, Consultation, and Pet controllers with CRUD operations. They manage the interactions between the user and the database entities
    
    Affected: ClientController, ConsultationController, PetController

---
+++ ClinicService/ClinicService/Controllers/ClientController.cs
@@ -0,0 +1,100 @@
+﻿using ClinicService.Models;
+using ClinicService.Models.Requests;
+using ClinicService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicService.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с клиентами клиники.
+    /// Предоставляет API для выполнения операций CRUD над клиентами.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ClientController : ControllerBase
+    {
+        private readonly IClientRepository _clientRepository;
+
+        /// <summary>
+        /// Конструктор контроллера клиентов.
+        /// </summary>
+        /// <param name="clientRepository">Репозиторий для работы с данными клиентов.</param>
+        public ClientController(IClientRepository clientRepository)
+        {
+            _clientRepository = clientRepository;
+        }
+
+        /// <summary>
+        /// Создает нового клиента в системе.
+        /// </summary>
+        /// <param name="createRequest">Данные нового клиента.</param>
+        /// <returns>Идентификатор созданного клиента.</returns>
+        [HttpPost("create")]
+        public ActionResult<int> Create([FromBody] CreateClientRequest createRequest)
+        {
+            int res = _clientRepository.Create(new Client
+            {
+                Document = createRequest.Document,
+                SurName = createRequest.SurName,
+                FirstName = createRequest.FirstName,
+                Patronymic = createRequest.Patronymic,
+                Birthday = createRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Обновляет данные существующего клиента.
+        /// </summary>
+        /// <param name="updateRequest">Обновленные данные клиента.</param>
+        /// <returns>Результат выполнения операции.</returns>
+        [HttpPut("update")]
+        public ActionResult<int> Update([FromBody] UpdateClientRequest updateRequest)
+        {
+            int res = _clientRepository.Update(new Client
+            {
+                ClientId = updateRequest.ClientId,
+                Document = updateRequest.Document,
+                SurName = updateRequest.SurName,
+                FirstName = updateRequest.FirstName,
+                Patronymic = updateRequest.Patronymic,
+                Birthday = updateRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Удаляет клиента по его идентификатору.
+        /// </summary>
+        /// <param name="clientId">Идентификатор клиента.</param>
+        /// <returns>Результат выполнения операции.</returns>
+        [HttpDelete("delete")]
+        public ActionResult<int> Delete(int clientId)
+        {
+            int res = _clientRepository.Delete(clientId);
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Получает список всех клиентов.
+        /// </summary>
+        /// <returns>Список клиентов.</returns>
+        [HttpGet("get-all")]
+        public ActionResult<List<Client>> GetAll()
+        {
+            return Ok(_clientRepository.GetAll());
+        }
+
+        /// <summary>
+        /// Получает клиента по его идентификатору.
+        /// </summary>
+        /// <param name="clientId">Идентификатор клиента.</param>
+        /// <returns>Данные клиента.</returns>
+        [HttpGet("get-by-id")]
+        public ActionResult<Client> GetById(int clientId)
+        {
+            return Ok(_clientRepository.GetById(clientId));
+        }
+    }
+}
---
+++ ClinicService/ClinicService/Controllers/PetController.cs
@@ -0,0 +1,96 @@
+﻿using ClinicService.Models;
+using ClinicService.Models.Requests;
+using ClinicService.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ClinicService.Controllers
+{
+    /// <summary>
+    /// Контроллер для работы с животными (питомцами) клиентов клиники.
+    /// Предоставляет API для выполнения операций CRUD над питомцами.
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PetController : ControllerBase
+    {
+        private readonly IPetRepository _petRepository;
+
+        /// <summary>
+        /// Конструктор контроллера питомцев.
+        /// </summary>
+        /// <param name="petRepository">Репозиторий для работы с данными питомцев.</param>
+        public PetController(IPetRepository petRepository)
+        {
+            _petRepository = petRepository;
+        }
+
+        /// <summary>
+        /// Создает нового питомца в системе.
+        /// </summary>
+        /// <param name="createRequest">Данные нового питомца.</param>
+        /// <returns>Идентификатор созданного питомца.</returns>
+        [HttpPost("create")]
+        public ActionResult<int> Create([FromBody] CreatePetRequest createRequest)
+        {
+            int res = _petRepository.Create(new Pet
+            {
+                ClientId = createRequest.ClientId,
+                Name = createRequest.Name,
+                Birthday = createRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Обновляет данные существующего питомца.
+        /// </summary>
+        /// <param name="updateRequest">Обновленные данные питомца.</param>
+        /// <returns>Результат выполнения операции.</returns>
+        [HttpPut("update")]
+        public ActionResult<int> Update([FromBody] UpdatePetRequest updateRequest)
+        {
+            int res = _petRepository.Update(new Pet
+            {
+                PetId = updateRequest.PetId,
+                ClientId = updateRequest.ClientId,
+                Name = updateRequest.Name,
+                Birthday = updateRequest.Birthday,
+            });
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Удаляет питомца по его идентификатору.
+        /// </summary>
+        /// <param name="petId">Идентификатор питомца.</param>
+        /// <returns>Результат выполнения операции.</returns>
+        [HttpDelete("delete")]
+        public ActionResult<int> Delete(int petId)
+        {
+            int res = _petRepository.Delete(petId);
+            return Ok(res);
+        }
+
+        /// <summary>
+        /// Получает список всех питомцев.
+        /// </summary>
+        /// <returns>Список питомцев.</returns>
+        [HttpGet("get-all")]
+        public ActionResult<List<Pet>> GetAll()
+        {
+            return Ok(_petRepository.GetAll());
+        }
+
+        /// <summary>
+        /// Получает питомца по его идентификатору.
+        /// </summary>
+        /// <param name="petId">Идентификатор питомца.</param>
+        /// <returns>Данные питомца.</returns>
+        [HttpGet("get-by-id")]
+        public ActionResult<Pet> GetById(int petId)
+        {
+            return Ok(_petRepository.GetById(petId));
+        }
+    }
+}
