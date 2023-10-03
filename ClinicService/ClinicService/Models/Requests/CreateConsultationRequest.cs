35dc52bd2ad2ec51e9e21f6dea62388f51f05acc
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-10-03 21:32:39 +0700

    feat: Integrate DTO requests
    #comment Create and Update requests for Client, Consultation, and Pet. They handle data transfer between the application and the database
    
    Affected: CreateClientRequest, CreateConsultationRequest, CreatePetRequest, UpdateClientRequest, UpdateConsultationRequest, UpdatePetRequest

---
+++ ClinicService/ClinicService/Models/Requests/CreateConsultationRequest.cs
@@ -0,0 +1,29 @@
+﻿namespace ClinicService.Models.Requests
+{
+    /// <summary>
+    /// Модель запроса для создания новой консультации.
+    /// Используется при передаче данных о консультации в API для её создания.
+    /// </summary>
+    public class CreateConsultationRequest
+    {
+        /// <summary>
+        /// Идентификатор клиента, для которого создаётся консультация.
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Идентификатор питомца, по поводу которого создаётся консультация.
+        /// </summary>
+        public int PetId { get; set; }
+
+        /// <summary>
+        /// Дата и время проведения консультации.
+        /// </summary>
+        public DateTime ConsultationDate { get; set; }
+
+        /// <summary>
+        /// Описание или причина консультации.
+        /// </summary>
+        public string Description { get; set; }
+    }
+}
---
+++ ClinicService/ClinicService/Models/Requests/CreatePetRequest.cs
@@ -0,0 +1,24 @@
+﻿namespace ClinicService.Models.Requests
+{
+    /// <summary>
+    /// Модель запроса для создания нового питомца.
+    /// Используется при передаче данных о питомце в API для его создания.
+    /// </summary>
+    public class CreatePetRequest
+    {
+        /// <summary>
+        /// Идентификатор клиента, которому принадлежит питомец.
+        /// </summary>
+        public int ClientId { get; set; }
+
+        /// <summary>
+        /// Имя питомца.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Дата рождения питомца.
+        /// </summary>
+        public DateTime Birthday { get; set; }
+    }
+}
