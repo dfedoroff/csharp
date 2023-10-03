d579c3a8c75b5fd772b06f90909392df8fde5a46
DmitryFedorov <shadowysupercoder@yandex.ru>
2023-10-03 21:20:55 +0700

    feat: Define repository interfaces
    #comment Define interfaces for Client, Consultation, and Pet repositories. They ensure that specific repositories adhere to certain contracts
    
    Affected: IClientRepository, IConsultationRepository, IPetRepository, IRepository

---
+++ ClinicService/ClinicService/Services/IPetRepository.cs
@@ -0,0 +1,12 @@
+﻿using ClinicService.Models;
+
+namespace ClinicService.Services
+{
+    /// <summary>
+    /// Интерфейс репозитория животных.
+    /// Определяет контракты для работы с данными о животных.
+    /// </summary>
+    public interface IPetRepository : IRepository<Pet, int>
+    {
+    }
+}
